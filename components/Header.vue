6910bb9f4f04774ed94a696d87586f5d4426b665
Danial-Zafar <mdanialzafar@gmail.com>
2022-01-13 08:44:31 +0000

    Made changes for mobile view

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,29 @@
   <nav class="bg-brand-black">
     <div class="max-w-full mx-auto py-6 px-4 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <div class="flex items-center justify-center lg:flex-1 sm:justify-start">
+          <div class="flex-shrink-0 flex items-center lg:w-0 lg:flex-1">
+            <img class="block lg:hidden h-8 w-auto" :src="require('~/assets/img/logo-white.png')" alt="Workflow">
+            <img class="hidden lg:block h-16 w-auto" :src="require('~/assets/img/logo-white.png')" alt="Workflow">
+          </div>
+          <div class="hidden sm:block sm:ml-14">
+            <div class="flex space-x-8 uppercase font-sans">
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <a href="#projects" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold" aria-current="projects">Projects</a>
+
+              <a href="#about" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">About</a>
+
+              <a href="#resources" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">Resources</a>
+
+              <a href="#insights" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">Insights</a>
+
+              <a href="#offices" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">Offices</a>
+
+              <a href="#contact" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">Contact</a>
+            </div>
+          </div>
+        </div>
+        <div class="-mr-2 inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
           <button type="button" @click="menuOpened = !menuOpened" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
@@ -28,28 +50,6 @@
             </svg>
           </button>
         </div>
-        <div class="flex-1 flex items-center justify-center sm:justify-start">
-          <div class="flex-shrink-0 flex items-center lg:w-0 lg:flex-1">
-            <img class="block lg:hidden h-8 w-auto" :src="require('~/assets/img/logo-white.png')" alt="Workflow">
-            <img class="hidden lg:block h-16 w-auto" :src="require('~/assets/img/logo-white.png')" alt="Workflow">
-          </div>
-          <div class="hidden sm:block sm:ml-14">
-            <div class="flex space-x-8 uppercase font-sans">
-              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="#projects" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold" aria-current="projects">Projects</a>
-
-              <a href="#about" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">About</a>
-
-              <a href="#resources" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">Resources</a>
-
-              <a href="#insights" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">Insights</a>
-
-              <a href="#offices" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">Offices</a>
-
-              <a href="#contact" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-semibold">Contact</a>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
 
@@ -57,17 +57,17 @@
     <div class="md:hidden" :style="menuOpened ? 'display: block' : 'display: none'" id="mobile-menu">
       <div class="px-2 pt-2 pb-3 space-y-1">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Projects</a>
+        <a href="#projects" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Projects</a>
 
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+        <a href="#about" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
 
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Resources</a>
+        <a href="#resources" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Resources</a>
 
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Insights</a>
+        <a href="#insights" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Insights</a>
 
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Offices</a>
+        <a href="#offices" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Offices</a>
 
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
+        <a href="#contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
       </div>
     </div>
   </nav>
