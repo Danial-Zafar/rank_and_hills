484fd0d2fed81e350cd253951dcac83bede7741d
Danial-Zafar <mdanialzafar@gmail.com>
2022-03-20 18:50:25 +0000

    Added Project Details with Mobile View

--- components/Project-details/project-details.ts
+++ components/Project-details/project-details.ts
@@ -13,7 +13,6 @@ export default class ProjectDetails extends Vue {
   imageUrl : string = ''
   pslug: string = ''
 
-   
   created () {
     this.pslug = '' + this.$route.query.pslug
 
@@ -43,6 +42,7 @@ export default class ProjectDetails extends Vue {
     try {
       const resp = (await this.$axios.get(`https://api.rankine-hill.com/project/${this.pslug}`)).data.result[0]
       this.projectDtails = resp
+      console.log('projectDtails', this.projectDtails)
 
       let matches: string = ''
       if (this.projectDtails.body_text) {
@@ -71,7 +71,28 @@ export default class ProjectDetails extends Vue {
       this.pslug = pslug.replace('https://www.rankine-hill.com/project/', '')
       this.getProjectDtailsContent()
     }
-    // div.relative>fa.absolute.bottom-0.left-0
-    // div.relative>fa.absolute.bottom-0.right-0
+  }
+
+  setImageClasses (index: number) {
+    if (index === 0) {
+      return 'col-span-full row-span-1'
+    } else if (index === 1 || index === 2) {
+      return 'col-span-1 row-start-2 row-end-3'
+    } else if (index === 3) {
+      return 'col-span-1 row-span-2'
+    } else if (index === 4) {
+      return 'col-start-2 row-end-4'
+    } else if (index === 5) {
+      return 'col-start-2 row-end-5'
+    } else {
+      return 'col-span-full'
+    }
+  }
+
+  nextSlide () {
+  }
+
+  prevSlide () {
+
   }
 }
